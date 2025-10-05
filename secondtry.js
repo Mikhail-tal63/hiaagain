4860f1153293cdb90427aced65fdac7560b75200
Jehad Mohamed <jjehad100@gmail.com>
2025-10-05 21:14:29 +0200

    first commit

---
+++ secondtry.js
@@ -0,0 +1,18 @@
+
+function sayHello(name) {
+  console.log(`👋 أهلاً ${name}! مرحباً بك في ورشة GitHub.`);
+}
+
+function showTime() {
+  const now = new Date();
+  console.log(`⏰ الوقت الآن هو: ${now.toLocaleTimeString()}`);
+}
+
+showTime();
+
+function sayBye(name) {
+  console.log(`👋 إلى اللقاء ${name}`);
+}
+sayBye("ليبيا");
+
+showTime();
